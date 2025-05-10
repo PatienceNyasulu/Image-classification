8eb77625fdd565a424cc1b1f41f8995f5d3c2c01
Czennie <83468952+PatienceNyasulu@users.noreply.github.com>
2025-05-10 08:55:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -25,6 +25,7 @@ st.write("Upload an image below (max 5MB). The model will predict its class.")
 
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
+
 if uploaded_file is not None:
     # Check file size (in MB)
     uploaded_file.seek(0, 2)
@@ -40,7 +41,13 @@ if uploaded_file is not None:
         # Preprocessing (Update this to match your CNN model's input shape)
         img = image.resize((224, 224))  # Modify size as required
         img_array = np.array(img) / 255.0
-        img_array = np.expand_dims(img_array, axis=0)
+        #img_array = np.expand_dims(img_array, axis=0)
+
+        if img_array.shape[-1] != 3:
+           st.error("Image is not in RGB format.")
+        else:
+           img_array = np.expand_dims(img_array, axis=0)  # Add batch dimension
+
 
         if st.button("Classify"):
             prediction = model.predict(img_array)
@@ -50,3 +57,4 @@ if uploaded_file is not None:
             class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer', 
           'dog', 'frog', 'horse', 'ship', 'truck']
             st.success(f"Predicted Class: **{class_names[class_idx]}**")
+            
